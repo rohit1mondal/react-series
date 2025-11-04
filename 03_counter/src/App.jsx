b94ca7e9e947d5fe224b04894d3c8f9d8931a6fc
Rohit Mondal <rohitmondal13112001@gmail.com>
2025-11-04 13:37:14 +0530

    counter project

---
+++ 03_counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+
+  // let counter = 15;
+
+  const addValue = () => {
+    counter = counter + 1;
+    setCounter(counter);
+    // console.log("value added", Math.random(), counter);
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
+  );
+}
+
+export default App;
